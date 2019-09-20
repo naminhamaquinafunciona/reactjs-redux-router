fd4336c4a52e0f9fff74fb293c3274f267e46f93
Newton Delbuque <delbuquenewton@gmail.com>
2019-09-20 13:35:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Model PWA
+
+This app demonstrates how to use ReactJS, and Service Worker to build a realtime PWA.
+
+## How to run this app
+
+1. Clone the repo - ``
+2. In the root of the project folder, run `npm install` to install all dependencies.
+
+## Built With
+
+...
+
+## Acknowledgements
+
+...
---
+++ src/pages/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../styles/logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Este é nosso primeiro projeto com ReactTJS
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
